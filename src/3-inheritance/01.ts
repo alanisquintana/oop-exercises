c7395770649777a45f4288a754decba338f2486f
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-06-26 22:14:50 -0300

    Inheritance

---
+++ src/3-inheritance/01.ts
@@ -0,0 +1,29 @@
+/* Write a TypeScript class called Animal with properties name and species. 
+Implement a constructor that initializes these properties when an Animal object 
+is created. Next, create a derived class Tiger that extends Animal. Add a 
+method roar() to the Tiger class that prints a message indicating that the 
+tiger is roaring. */
+
+class Animal {
+    name: string
+    species: string
+
+    constructor(name: string, species: string) {
+        this.name = name
+        this.species = species
+    }
+}
+
+class Tiger extends Animal{
+
+    constructor(name: string, species: string) {
+        super(name, species)
+    }
+
+    roar() {
+        console.log(`The tiger named ${this.name} from the species ${this.species} is roaring.`)
+    }
+}
+
+const myTiger = new Tiger("Manson", "Bengal Tiger")
+myTiger.roar()
---
+++ src/3-inheritance/02.ts
@@ -0,0 +1,23 @@
+/* Write a TypeScript class called Person with properties name and age. 
+Implement a constructor that initializes these properties when a Person object 
+is created. Then, create a derived class Client that extends Person. Override 
+the constructor of the Client class to include an additional property id. */
+
+class Person {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+}
+
+class Client extends Person {
+    id: number
+
+    constructor(name: string, age: number, id: number) {
+        super(name, age)
+        this.id = id
+    }
+}
