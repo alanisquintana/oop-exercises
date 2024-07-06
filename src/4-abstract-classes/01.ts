387f7ad0f97dda5e787527b4b1b3a306457ffb4e
Alanis Quintana <alanisquintana.dev@gmail.com>
2024-07-05 23:53:35 -0300

    Abstract Classes

---
+++ src/4-abstract-classes/01.ts
@@ -0,0 +1,29 @@
+// Write a TypeScript program that creates an abstract class called Formats with properties like color and an abstract method getPerimeter(). Implement derived classes for specific formats (e.g., Circle, Rectangle) that extend Formats and provide concrete implementations for calculating their perimeters.
+
+    abstract class Formats {
+        color: string
+
+        constructor(color: string) {
+            this.color = color
+        }
+
+        abstract getPerimeter(): number
+    }
+
+    class Rectangle extends Formats {
+        width: number
+        height: number
+        
+        constructor(color: string, width: number, height: number) {
+            super(color)
+            this.width = width
+            this.height = height
+        }
+
+        getPerimeter(): number {
+            return 2 * (this.width + this.height)
+        }
+    }
+
+    const myRectangle = new Rectangle("Purple", 20, 10)
+    console.log(`My ${myRectangle.color} rectangle is ${myRectangle.getPerimeter()}cm.`)
---
+++ src/4-abstract-classes/03.ts
@@ -0,0 +1,30 @@
+// Write a TypeScript program that defines an abstract class Employee with properties such as name, employeeId, and an abstract method calculateSalary(). Create derived classes (e.g., FullTimeEmployee, PartTimeEmployee) that extend Employee and provide salary calculation logic based on employment type
+
+abstract class EmployeeSalary {
+    name: string
+    employeeId: number
+    
+
+    constructor(name: string, employeeId: number, ) {
+        this.name = name
+        this.employeeId = employeeId
+    }
+
+    abstract calculateSalary(): number
+}
+
+class Stagiary extends EmployeeSalary {
+    salary: number
+
+    constructor(name: string, employeeId: number) {
+        super(name, employeeId)
+        this.salary = 1500
+    }
+
+    calculateSalary(): number {
+        return this.salary + 200 + 150
+    }
+}
+
+const myStagiary = new Stagiary("George", 239753)
+console.log(myStagiary.calculateSalary())
